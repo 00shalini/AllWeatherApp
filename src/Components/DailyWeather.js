bcb03234d149adad8f23cbfc0ad43e621359d498
00Shalini <shalini07061997@gmail.com>
2022-07-02 07:55:36 +0530

    scrollable weather data

---
+++ src/Components/DailyWeather.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import cloudy from '../Components/Images/cloudy.png';
+import storm from '../Components/Images/storm.png';
+import partlysunny from '../Components/Images/partlysunny.png';
+import rainy from '../Components/Images/rainy.png';
+import sunny from '../Components/Images/sunny.png';
+import '../Components/DailyWeather.css';
+
+const DailyWeather = ({dailyData,date}) => {
+ 
+    
+ 
+    const [currentDay, setCurrentDay]= useState([]);
+    const [icon, setIcon] = useState([]);
+
+
+    const getGivenDay = (date) => {
+       
+        const givenDate = new Date(date);
+        const day = givenDate.getDay();
+
+        switch(day) {
+            case 0: setCurrentDay("Sun");
+            break;
+            case 1: setCurrentDay("Mon");
+            break;
+            case 2: setCurrentDay("Tue");
+            break;
+            case 3: setCurrentDay("Wed");
+            break;
+            case 4: setCurrentDay("Thu");
+            break;
+            case 5: setCurrentDay("Fri");
+            break;
+            case 6: setCurrentDay("Sat");
+            break;
+            default:
+                setCurrentDay("default");
+            break;
+        }
+      
+    }
+
+    const getIcon = (icon) => {
+        switch(icon) {
+            case "Thunderstorms": setIcon(storm);
+            break;
+            case "Rainy": setIcon(rainy);
+            break;
+            case "Partly sunny" : setIcon(partlysunny);
+            break;
+            case "Cloudy": setIcon(cloudy);
+            break;
+            case "Mostly cloudy": setIcon(cloudy);
+            break;
+            case "Sunny": setIcon(sunny);
+            break;
+            default:
+                setIcon(sunny);
+                break;
+
+        }
+    }
+
+    const handleWeatherGraph = () => {
+        
+    }
+    
+    useEffect(() => {
+        
+        getGivenDay(date);
+        getIcon(dailyData.Day.IconPhrase);
+    });
+
+    return (
+        <div className="dailyWeather" onClick={handleWeatherGraph}>
+        
+       
+            <div className="weekDay">{currentDay}</div>
+            {console.log(dailyData)}
+            <div className="temp"><p className="temper">{(((dailyData.Temperature.Maximum.Value)-32)*5/9).toFixed(0)}° </p>     <p className="temper"> {(((dailyData.Temperature.Minimum.Value)-32)*5/9).toFixed(0)}°</p></div>
+            <div>
+            <img src={icon} alt="weather icon" className="weatherIcon"/>
+            </div>
+            <div className="weather">{dailyData.Day.IconPhrase}</div>
+        
+        </div>
+    )
+}
+
+export default DailyWeather;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,16 +1,111 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import "../Components/SearchBar.css"; 
 import map from '../Components/Images/map.png';
 import search from '../Components/Images/search.png';
+import axios from 'axios';
+import DailyWeather from "./DailyWeather";
+
 
 const SearchBar = () => {
+
+   const apiKey = "njP35ATXQiw53GM6G6lLUUci7pppyyt9";
+   
+   const [weatherData, setWeatherData] = useState();
+   const [data, setData] = useState();
+   const [city, setCity] = useState("");
+   const [dailyData, setDailyData] = useState([]);
+
+
+   const getWeatherDetails = (city) => {
+    if (!city) return
+    const apiUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`;
+    
+    axios.get(apiUrl)
+    .then((res) => {
+       // console.log(res.data[0])
+        getCurrentWeather(res.data[0].Key);
+        getDailyWeatherDetails(res.data[0].Key);
+        setWeatherData(res.data[0])
+    }).catch((error) =>{
+        console.log(error);
+    })
+
+   };
+   
+   const getCurrentWeather = (Key) => {
+    axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${apiKey}`)
+    .then((res) => {
+       // console.log(res.data[0])
+        setData(res.data[0])
+    })
+   }
+
+   const getDailyWeatherDetails = (Key) => {
+    
+   const apiUrl = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${Key}?apikey=${apiKey}`;
+    axios.get(apiUrl)
+    .then((res) => {
+     //  console.log(res.data)
+        setDailyData(res.data)
+    }).catch((error) =>{
+        console.log(error);
+    })
+   }
+
+ const handleChange = (e) => {
+    e.preventDefault();
+//     fetch(`${ApiUrl}/cities?namePrefix=${e.target.value}`, geoApioptions)
+//     .then((data) => {
+//      data.json().then((resp) => {
+//         //  return {
+//         //      options: resp.data.map((city) => {
+//         //          return {
+//         //              value: `${city.latitude} ${city.longitude}`,
+//         //              label: `${city.name} ${city.countryCode}`,
+//         //          };
+//         //      }),
+//         //  };
+//         console.log(resp.data)
+         
+//     }).catch(err => console.log(err));
+//  })
+  
+    setCity(e.target.value)
+ 
+}
+
+
+   const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+        getWeatherDetails(city);
+        
+        // dailyData?.forecast?.forecastday?.map((item) =>  {
+        //     handleDay(item.date)
+        // })
+       
+      }
+   }
+   
+
     return (
         <div className="searchbar">
            
             <img src={map} alt="location" className="location"/>
             <img src={search} alt="search" className="search"/>
-            <input type="text" value="" className="inputlocation"/>
+            
+            <input type="text" value={city} onChange={handleChange} className="inputlocation" onKeyDown={handleKeyDown}/>
+            {/* {console.log(dailyData)} */}
+            <div className="weatherCard">
+            {dailyData?.DailyForecasts?.map((item) => {
+               // console.log(item.Date)
+                 return  <DailyWeather dailyData={item} date={item.Date}/>  
+            })
+            }
+                
+                </div>
            
+            {/* <p>{data?.name} </p>
+            <p>{((data?.main.temp)-273.15).toFixed(2)} °C</p> */}
         </div>
     )
 }
